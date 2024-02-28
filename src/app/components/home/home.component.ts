f8513b9c7c45206115051b84d641a3a5b0066721
joaquincorimayo <joaquin97corimayo@gmail.com>
2024-02-28 11:13:15 -0300

    feat: add template structure

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TableModule} from "primeng/table";
 import {MessagesModule} from "primeng/messages";
 import {TooltipModule} from "primeng/tooltip";
+import {CheckboxModule} from "primeng/checkbox";
+import {MenuItem} from "primeng/api";
+import {MenubarModule} from "primeng/menubar";
+
+interface TestCase {
+  testActions: string;
+  expectedOutput: string;
+  actualOutput: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -9,11 +18,45 @@ import {TooltipModule} from "primeng/tooltip";
   imports: [
     TableModule,
     MessagesModule,
-    TooltipModule
+    TooltipModule,
+    CheckboxModule,
+    MenubarModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  testCases: TestCase[] = [];
+
+  items: MenuItem[] = [];
+
+  ngOnInit(): void {
+    this.getItems();
+    this.addRow();
+  }
+
+  getItems(): void {
+    this.items = [{
+      label: 'New row',
+      icon: 'pi pi-fw pi-plus',
+      command: () => this.addRow()
+    }, {
+      label: 'Delete row',
+      icon: 'pi pi-fw pi-trash',
+      command: () => this.deleteRow()
+    }];
+  }
+
+  private addRow(): void {
+    this.testCases.push({
+      testActions: '',
+      expectedOutput: '',
+      actualOutput: ''
+    });
+  }
+
+  private deleteRow(): void {
+    this.testCases.pop();
+  }
 }

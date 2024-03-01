cb7309940d0fbd659f90635a14ea7a04aecd11eb
joaquincorimayo <joaquin97corimayo@gmail.com>
2024-03-01 16:49:31 -0300

    refactor: new structure for the table

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,10 +3,14 @@ import {TableModule} from "primeng/table";
 import {MessagesModule} from "primeng/messages";
 import {TooltipModule} from "primeng/tooltip";
 import {CheckboxModule} from "primeng/checkbox";
-import {MenuItem} from "primeng/api";
 import {MenubarModule} from "primeng/menubar";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {FormsModule} from "@angular/forms";
 
 interface TestCase {
+  step: number;
   testActions: string;
   expectedOutput: string;
   actualOutput: string;
@@ -21,42 +25,41 @@ interface TestCase {
     TooltipModule,
     CheckboxModule,
     MenubarModule,
+    RadioButtonModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-
-  testCases: TestCase[] = [];
-
-  items: MenuItem[] = [];
+  testCasesList : TestCase[] = [];
+  testResult: any;
 
   ngOnInit(): void {
-    this.getItems();
+    this.addRow();
+    this.addRow();
+    this.addRow();
     this.addRow();
   }
 
-  getItems(): void {
-    this.items = [{
-      label: 'New row',
-      icon: 'pi pi-fw pi-plus',
-      command: () => this.addRow()
-    }, {
-      label: 'Delete row',
-      icon: 'pi pi-fw pi-trash',
-      command: () => this.deleteRow()
-    }];
-  }
-
-  private addRow(): void {
-    this.testCases.push({
+  addRow(): void {
+    const newTestCase: TestCase = {
+      step: this.testCasesList.length + 1,
       testActions: '',
       expectedOutput: '',
       actualOutput: ''
-    });
+    };
+    this.testCasesList.push(newTestCase);
   }
 
-  private deleteRow(): void {
-    this.testCases.pop();
+  deleteRow(): void {
+    this.testCasesList.pop();
   }
+
+  clearTable():void  {
+    this.testCasesList.splice(0, this.testCasesList.length);
+  }
+
 }
